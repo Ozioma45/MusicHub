45d99cb065c637abd28ea7228de79472638684c4
Ozioma45 <oziomaegole@gmail.com>
2025-07-15 03:10:41 +0100

    auth data sent to db

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,28 @@
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { prisma } from "@/lib/db";
+import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
   const user = await currentUser();
   console.log(user);
+  if (!user) {
+    redirect("/");
+  }
+
+  const loggedUser = await prisma.user.findUnique({
+    where: { clerkUserId: user.id },
+  });
+
+  if (!loggedUser) {
+    await prisma.user.create({
+      data: {
+        clerkUserId: user.id,
+        name: `${user.firstName} ${user.lastName}`,
+        imageUrl: user.imageUrl,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+  }
   return (
     <div className="p-6">
       <h1 className="text-xl font-bold">
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { Music } from "lucide-react";
+import { SignedOut, SignInButton, UserButton, SignedIn } from "@clerk/nextjs";
+import React from "react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <div className="sticky top-0 z-30 border-b bg-background px-4 sm:px-6">
+      <div className="flex items-center justify-between mx-auto max-w-4xl h-16">
+        <div className="flex gap-4">
+          <Link href="/" className="flex items-center gap-2">
+            <Music className="h-6 w-6" />
+            <span className="font-bold">Music Hub.</span>
+          </Link>
+          <nav>
+            <Link
+              href="/dashboard"
+              className="text-sm font-medium text-muted-foreground transition-color hover:text-foreground"
+            >
+              Dashboard
+            </Link>
+          </nav>
+        </div>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button variant="outline">Sign in</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
