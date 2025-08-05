115c50864ee3b1382dc079247db7f472ff79ceb0
Ozioma45 <oziomaegole@gmail.com>
2025-08-05 15:54:00 +0100

    added the cloudinary link to the next.config

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "randomuser.me",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
