daa9ba07ed227a4939e4bc9c3db25099e7aae923
Ozioma45 <oziomaegole@gmail.com>
2025-08-27 01:25:02 +0100

    updated the landing page

--- src/components/landing/CtaSection.tsx
+++ src/components/landing/CtaSection.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function CallToActionSection() {
   return (
     <section className="bg-sky-600 text-white py-16 px-6 text-center rounded-lg max-w-6xl mx-auto">
-      <h2 className="text-3xl font-bold mb-2">Ready to Connect with Music?</h2>
+      <h2 className="text-3xl font-bold mb-2">
+        Ready to Connect with Musicians ?
+      </h2>
       <p className="mb-6 text-lg">
         Join our vibrant community of musicians and music lovers today.
       </p>
